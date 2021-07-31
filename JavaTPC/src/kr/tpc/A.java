421e6bae42656994ee4dccf2a1b764b4b9477ca4
schw240 <54871612+schw240@users.noreply.github.com>
2021-07-31 18:35:13 +0900

    Object 클래스 활용

---
+++ JavaTPC/src/kr/tpc/A.java
@@ -0,0 +1,7 @@
+package kr.tpc;
+
+public class A {
+	public void go() {
+		System.out.println("A의 go메서드입니다.");
+	}
+}
---
+++ JavaTPC/src/kr/tpc/B.java
@@ -0,0 +1,7 @@
+package kr.tpc;
+
+public class B {
+	public void go() {
+		System.out.println("B의 go메서드입니다.");
+	}
+}
